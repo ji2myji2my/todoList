08548c056074ac3383c8ef445202a48d3795cf68
Jimmy <jimmy.mennock@gmail.com>
2025-03-07 09:47:04 +0100

    Starting of setUp dark and light mode management

---
+++ config/theme.js
@@ -0,0 +1,30 @@
+export const colors = {
+
+    light: {
+        backgroundColor: '#E8EAED',
+
+
+        // primary: "#f3f4f6",
+        // secondary: "#ffffff",
+        // terciary: "#4b5563",
+        // accent: "#0891b2",
+        // tint: "#111827"
+    },
+
+    dark: {
+        backgroundColor: '#17202a',
+
+
+        // primary: "#1f2937",
+        // secondary: "#111827",
+        // terciary: "#d1d5bd",
+        // accent: "#0891b2",
+        // tint: "f9fafafb"
+    },
+
+    // primary: "#f3f4f6",
+    // secondary: "#ffffff",
+    // terciary: "#4b5563",
+    // accent: "#0891b2",
+    // tint: "#111827"
+};
